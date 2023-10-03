baa5e9425db2b80aa4bb68a0b0f6f6c63259a7db
Ibrahim Najjar <ABnajjar@hotmail.com>
2023-10-04 00:53:53 +0200

    add application menu with help submenu
    * moved version information into help menu away from title

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"embed"
 	"fmt"
+	"runtime"
 
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/menu"
+	"github.com/wailsapp/wails/v2/pkg/menu/keys"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	wailsRuntime "github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 //go:embed all:frontend/dist
@@ -20,10 +24,30 @@ var BuildLink string = "http://localhost"
 func main() {
 	app := NewApp()
 
+	AppMenu := menu.NewMenu()
+
+	FileMenu := AppMenu.AddSubmenu("File")
+	FileMenu.AddText("Quit", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
+		wailsRuntime.Quit(app.ctx)
+	})
+
+	if runtime.GOOS == "darwin" {
+		AppMenu.Append(menu.EditMenu())
+	}
+
+	HelpMenu := AppMenu.AddSubmenu("Help")
+	HelpMenu.AddText("About", keys.CmdOrCtrl("h"), func(_ *menu.CallbackData) {
+		wailsRuntime.MessageDialog(app.ctx, wailsRuntime.MessageDialogOptions{
+			Title:   "About",
+			Message: fmt.Sprintf("Swervo %s\nBuilt @ %s\nCommit SHA: %s\nBuild Link: %s", Version, BuildTimestamp, CommitSha, BuildLink),
+		})
+	})
+
 	err := wails.Run(&options.App{
-		Title:  fmt.Sprintf("Swervo - %s - %s - %s", Version, BuildTimestamp, CommitSha),
+		Title:  "Swervo",
 		Width:  1024,
 		Height: 768,
+		Menu:   AppMenu,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
