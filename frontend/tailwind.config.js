b988aa18b116348d9c43fb32c557c4e6291bdd4e
Ibrahim Najjar <ABnajjar@hotmail.com>
2023-10-04 00:10:41 +0200

    basic support for light and dark mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media",
   theme: {
     debugScreens: {
       prefix: "dbg-screen: ",
       position: ["bottom", "right"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        "skin-primary": "rgb(var(--color-primary) / <alpha-value>)",
+        "skin-secondary": "rgb(var(--color-secondary) / <alpha-value>)",
+      },
+    },
   },
   plugins: [require("tailwindcss-debug-screens")],
 };
