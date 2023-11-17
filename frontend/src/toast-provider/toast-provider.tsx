d3b5855704c3a48f303a405b4b04b334245d8edc
Ibrahim Najjar <ABnajjar@hotmail.com>
2023-11-17 20:38:34 +0100

    toast container listens to toast provider for rendering toasts
    * prevents infinite recursive renders since producers of toasts depend on the toast provider and end up re-rendering because their context has changed

--- frontend/src/toast-provider/toast-provider.tsx
+++ frontend/src/toast-provider/toast-provider.tsx
@@ -1,5 +1,10 @@
-import { useRef, useState } from "react"
-import { ToastContext, ToastOptions, ToastSpec } from "./toast-context"
+import { useRef } from "react"
+import {
+  ToastContainer,
+  ToastContext,
+  ToastOptions,
+  ToastSpec,
+} from "./toast-context"
 import { Toast } from "../components/toast"
 
 export function ToastProvider({ children }: { children: React.ReactNode }) {
@@ -8,20 +13,22 @@ export function ToastProvider({ children }: { children: React.ReactNode }) {
   const toastId = useRef(1)
 
   const toastQueue = useRef<ToastSpec[]>([])
-  const [toasts, setToasts] = useState<React.ReactElement<ToastSpec>[]>([])
+  const toasts = useRef<React.ReactElement<ToastSpec>[]>([])
+  const notifyContainer = useRef<ToastContainer | null>(null)
 
   function addToast(toast: ToastSpec) {
-    if (toasts.length < MaxToasts) {
-      setToasts((toasts) => [
-        ...toasts,
+    if (toasts.current.length < MaxToasts) {
+      toasts.current.push(
         <Toast
           key={toast.id}
           {...toast}
         />,
-      ])
+      )
     } else {
       toastQueue.current.push(toast)
     }
+
+    if (notifyContainer.current) notifyContainer.current(toasts.current)
   }
 
   function removeToast(id: string) {
@@ -34,10 +41,13 @@ export function ToastProvider({ children }: { children: React.ReactNode }) {
         />
       )
 
-      setToasts((toasts) => [...toasts.filter((toast) => toast.key !== id), toastEl])
+      toasts.current = toasts.current.filter((toast) => toast.key !== id)
+      toasts.current.push(toastEl)
     } else {
-      setToasts((toasts) => toasts.filter((toast) => toast.key !== id))
+      toasts.current = toasts.current.filter((toast) => toast.key !== id)
     }
+
+    if (notifyContainer.current) notifyContainer.current(toasts.current)
   }
 
   function showInfo(opts: string | ToastOptions) {
@@ -80,7 +90,9 @@ export function ToastProvider({ children }: { children: React.ReactNode }) {
   return (
     <ToastContext.Provider
       value={{
-        toasts: toasts,
+        iRenderToasts: (fn) => {
+          notifyContainer.current = fn
+        },
         showInfo,
         showSuccess,
         showWarning,
